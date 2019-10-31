0209a3cd231b1dccd8c999a7bc0b16369f1492ef
U-AMDX34GB-PC\papa <muh_kosta@mail.ru>
2019-10-31 19:31:52 +0300

    new work2

--- client.cpp
+++ client.cpp
@@ -11,35 +11,23 @@
 using namespace rapidjson;
 using namespace std;
 // Используем библиотеку RapidJson
- char* WriteJsonInfoAboutWindow(const char* key_as_window_name,const char* value_as_window_class) {
+StringBuffer WriteJsonInfoAboutWindow(const char* key_as_window_name, const char* value_as_window_class) {
     StringBuffer s;
     Writer<StringBuffer> writer(s);
-    writer.StartObject(); 
-    writer.Key(key_as_window_name); 
-    writer.String(value_as_window_class); 
+    writer.StartObject();
+    writer.Key(key_as_window_name);
+    writer.String(value_as_window_class);
     writer.EndObject();
-    return (char*)s.GetString();
-}
-vector<string> GetActiveWindowTitleAndClassName() {
-    vector<string> list_about_window;
-    char wnd_title[256];
-    char wnd_class_name[256];
-    HWND hwnd = GetForegroundWindow(); 
-    GetWindowText(hwnd, wnd_title, sizeof (wnd_title));
-    GetClassName(hwnd, wnd_class_name, sizeof (wnd_class_name));
-    list_about_window.push_back(string(wnd_title));
-    list_about_window.push_back(string(wnd_class_name));
-    return list_about_window;
+    return  s;
 }
 using namespace std;
-// переодичность сбора - 1
-// адресс сервера - 2
-// порт у сервера - 3
+// адресс сервера - 1
+// порт у сервера - 2
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        cout << "We must have 3 parameters."<<endl;
-        cout << "Usage:<program> <rate to ask> <addres to server> <servers port>"<<endl;
-		cout<<"Server is on port 25001"<<endl;
+    if (argc != 3) {
+        cout << "We must have 2 parameters." << endl;
+        cout << "Usage: <program>  <addres to server> <servers port>" << endl;
+        cout << "Server is on port 25001" << endl;
         return EXIT_FAILURE;
     }
     cout << "CL START" << '\n';
@@ -48,13 +36,12 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in peer;
     SOCKET s;
     int rc;
-    string window_name;
-    string window_class;
-    vector<string>common_window_info;
+    char wnd_title[256];
+    char wnd_class_name[256];
     char* json_str;
     peer.sin_family = AF_INET;
-    peer.sin_port = htons(atoi(argv[3]));
-    peer.sin_addr.s_addr = inet_addr(argv[2]); // 127.0.0.1 - адресс сервера
+    peer.sin_port = htons(atoi(argv[2]));
+    peer.sin_addr.s_addr = inet_addr(argv[1]); // 127.0.0.1 - адресс сервера
     s = socket(AF_INET, SOCK_STREAM, 0);
     if (s < 0) {
         perror("Could not create socket.");
@@ -62,22 +49,24 @@ int main(int argc, char *argv[]) {
     }
     rc = connect(s, (struct sockaddr *) &peer, sizeof (peer));
     if (rc) {
-        perror("Could not connect");
-        closesocket(s);
+        perror("Could not connect - Server is unavailable.Launch the Server.");
+        return EXIT_FAILURE;
     }
-	int client_running=1;
+    int client_running = 1;
     while (client_running) {
-        common_window_info = GetActiveWindowTitleAndClassName();
-        window_name = common_window_info.at(0);
-        window_class = common_window_info.at(1);
-        json_str = WriteJsonInfoAboutWindow(window_name.c_str(), window_class.c_str());
-        cout<<json_str<<endl;
-        rc = send(s, json_str, sizeof(json_str), 0);
+        HWND hwnd = GetForegroundWindow();
+        GetWindowText(hwnd, wnd_title, sizeof (wnd_title));
+        GetClassName(hwnd, wnd_class_name, sizeof (wnd_class_name));
+        StringBuffer json_expr=WriteJsonInfoAboutWindow(wnd_title, wnd_class_name);
+        rc = send(s,json_expr.GetString(), json_expr.GetSize(), 0);
+        cout << "json " <<json_expr.GetString()<< endl;
         if (rc <= 0) {
             perror("Could not send data");
+            perror("Maybe Server connection lost");
             closesocket(s);
+            break;
         }
-        Sleep(atoi(argv[1]));
+        Sleep(1000);
     }
     WSACleanup();
     system("PAUSE");
--- server.cpp
+++ server.cpp
@@ -1,12 +1,18 @@
 #include <cstdlib>
 #include <iostream>
 #include <winsock2.h>
-#include <stdio.h>
 #include <string.h>
 #include <windows.h>
 #include <synchapi.h>
+#include <fstream>
 using namespace std;
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cout << "We must have 1 parameters." << endl;
+        cout << "Usage: <program> <name log file>" << endl;
+        cout << "Server is on port 25001" << endl;
+        return EXIT_FAILURE;
+    }
     cout << "SR START" << '\n';
     WSAData WSData;
     WSAStartup(0x101, &WSData);
@@ -15,6 +21,7 @@ int main(int argc, char *argv[]) {
     int s1;
     int rc;
     char buf [1024];
+    ofstream fout(argv[1]); // имя файла куда записывать
     local.sin_family = AF_INET;
     local.sin_port = htons(25001);
     local.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -25,7 +32,7 @@ int main(int argc, char *argv[]) {
         perror("Could not bind address");
         closesocket(s);
     }
-    int server_running=1; 
+    int server_running = 1;
     while (server_running) {
         rc = listen(s, 5);
         if (rc) {
@@ -38,13 +45,16 @@ int main(int argc, char *argv[]) {
             closesocket(s);
         }
         rc = recv(s1, buf, 1024, 0);
+        buf[rc]='\0';
         if (rc <= 0) {
             perror("Cannot read data.");
             closesocket(s);
         }
+        fout << buf;
         cout << buf << '\n';
-        Sleep(7);
+        Sleep(1000);
     }
+    fout.close();
     closesocket(s);
     WSACleanup();
     system("PAUSE");
